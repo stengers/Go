7535e790701434a248e5b04ecb3eac958088dce5
stengers <spencerstenger@gmail.com>
2017-05-10 13:51:50 -0700

    Adding constant declaration

---
+++ src/tutorials/constants.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+//decalre a constant with the const declaration
+const Pi = 3.14
+
+func main() {
+	const World = "世界"
+	fmt.Println("Hello", World)
+	fmt.Println("Happy", Pi, "day!")
+	
+	const Truth = true
+	fmt.Println("Go is awesome?", Truth)
+}
