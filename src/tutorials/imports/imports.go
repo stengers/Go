eca7a10ff435a800eddb7543ead381eb0dc7c90b
stengers <spencerstenger@gmail.com>
2017-05-10 11:37:31 -0700

    Adding import practice package

---
+++ src/tutorials/imports/imports.go
@@ -0,0 +1,13 @@
+package main
+
+//import "fmt"
+//import "math"
+//grouping imports rather than importing each one on a separate line
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+}
